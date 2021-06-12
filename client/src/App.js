b1323bb1937b91fcb1ffbcfab2ab14de4993454a
Shah Arafat <shaharafhat@gmail.com>
2021-06-12 16:54:26 +0600

    react setup done

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Booklist from "./components/Booklist";
+
+function App() {
+	return (
+		<div className="main">
+			<Booklist />
+		</div>
+	);
+}
+
+export default App;
